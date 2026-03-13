5e8623ca29485db5ea3ae0b66e09e5f3a3a2e39d
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-13 12:03:48 -0400

    cargando

---
+++ Actividad_7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Buscador de Objetos de la Casa ---");
+            Console.WriteLine();
+
+ 
+            Dictionary<string, string> miCasa = new Dictionary<string, string>();
+
+            miCasa.Add("Estufa", "Cocina");
+            miCasa.Add("Cama", "Habitacion");
+            miCasa.Add("Sofa", "Sala");
+            miCasa.Add("Espejo", "Baño");
+
+         
+            string objetoABuscar = "Estufa";
+
+            Console.WriteLine("Buscando en la casa: " + objetoABuscar);
+
+    
+            if (miCasa.ContainsKey(objetoABuscar))
+            {
+                Console.WriteLine("Resultado: Claro! La " + objetoABuscar + " esta registrada en el sistema.");
+            }
+            else
+            {
+                Console.WriteLine("Resultado: No se encontro ese objeto en la lista.");
+            }
+
+        
+            Console.ReadKey();
+        }
+    }
+}
