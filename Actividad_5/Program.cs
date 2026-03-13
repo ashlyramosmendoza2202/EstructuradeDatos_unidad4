13ec4079ea616b8914d503ae7e819b444d65cc9a
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-13 11:25:00 -0400

    cargando

---
+++ Actividad_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiTareaEjercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Lista de Frutas (Solo Nombres) ---");
+            Console.WriteLine();
+
+            Dictionary<string, string> misFrutas = new Dictionary<string, string>();
+
+            misFrutas.Add("Manzana", "Roja");
+            misFrutas.Add("Banana", "Amarilla");
+            misFrutas.Add("Uva", "Morada");
+            misFrutas.Add("Pera", "Verde");
+            misFrutas.Add("Naranja", "Anaranjada");
+
+            Console.WriteLine("Las frutas en mi diccionario son:");
+
+            
+            foreach (string fruta in misFrutas.Keys)
+            {
+                Console.WriteLine("- " + fruta);
+            }
+
+       
+            Console.ReadKey();
+        }
+    }
+}
