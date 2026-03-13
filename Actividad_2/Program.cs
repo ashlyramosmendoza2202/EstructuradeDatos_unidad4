d0542240d22a6fd720754fadaa5db4c6f7c42475
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-13 10:23:32 -0400

    cargando

---
+++ Actividad_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Precios de Productos ---");
+            Console.WriteLine();
+
+            Dictionary<string, double> misProductos = new Dictionary<string, double>();
+
+            misProductos.Add("Pizza", 500.00);
+            misProductos.Add("Refresco", 65.50);
+            misProductos.Add("Hamburguesa", 250.00);
+
+           
+            Console.WriteLine("El precio del producto Refresco es: $" + misProductos["Refresco"]);
+
+         
+            Console.WriteLine();
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
