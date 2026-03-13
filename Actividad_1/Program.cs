ef642b7a8b91b1a041e52347350b7392f96d3f3d
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-13 10:14:49 -0400

    cargando

---
+++ Actividad_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> paises = new Dictionary<string, string>();
+            paises.Add("República Dominicana", "Santo Domingo");
+            paises.Add("Colombia", "Bogota");
+            paises.Add("Haiti", "Puerto Principe");
+
+            Console.WriteLine("--- Aqui lista de Países y Capitales ---");
+            foreach (var elemento in paises)
+            {
+                Console.WriteLine("País: " + elemento.Key + " | Capital: " + elemento.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
