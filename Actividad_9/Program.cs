86e068fc5eabde56413db92733061ba5eaba7b96
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-13 12:26:52 -0400

    cargando

---
+++ Actividad_9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Calculo Total de Gastos ---");
+        Console.WriteLine();
+
+        Dictionary<string, int> misGastos = new Dictionary<string, int>();
+
+        misGastos.Add("Internet", 1200);
+        misGastos.Add("Luz", 1500);
+        misGastos.Add("Agua", 300);
+        misGastos.Add("Basura", 150);
+
+   
+        int sumaTotal = 0;
+
+      
+        foreach (int monto in misGastos.Values)
+        {
+            sumaTotal = sumaTotal + monto;
+        }
+
+        Console.WriteLine("El total de los gastos de la casa es: $" + sumaTotal);
+
+        Console.ReadKey();
+    }
+}
